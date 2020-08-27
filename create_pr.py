0ef3f6bb5464b29323769746be00ab0b28014edb
Floriani <carlos.floriani@sci10.com.br>
2020-08-27 17:44:22 -0300

    teste label

--- create_pr.py
+++ create_pr.py
@@ -18,15 +18,20 @@ for cont in range(0,len(name_issue)):
 title_pr = 'WIP: ' + name_issue
 
 # # cria o pr
-# try:
-#     repo.create_pull(title=title_pr, body='#' + num_issue, head=os.getenv('user') + ':' + name_issue, base='master')
-#     repo.create_label(name='pr-teste',color='e53242',description='test label')
-# except Exception as erro:
-#     print('ERRO: Aconteceu o erro abaixo')
-#     print(erro)
+try:
+    repo.create_pull(title=title_pr, body='#' + num_issue, head=os.getenv('user') + ':' + name_issue, base='master')
+    for pull_request in repo.get_pulls(state='open'):
+        if pull_request.title == title_pr:
+            pr = repo.get_pull(pull_request.number)
+            pr.add_to_labels('pr: em andamento')
+            break
+except Exception as erro:
+    print('ERRO: Aconteceu o erro abaixo')
+    print(erro)
 
 
-pr = repo.get_pull(9)
+pr = repo.get_pulls()
+pr.add_to_labels('pr-teste')
 print(pr.labels)
 for label in pr.get_labels():
     print(label.name)
